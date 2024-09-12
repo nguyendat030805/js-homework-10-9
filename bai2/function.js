52a52bd08ebc2b1f2209e1d4113ac66e43c3922c
nguyendat030805 <dat656473@gmail.com>
2024-09-12 10:29:42 +0700

    bai tap

---
+++ bai2/function.js
@@ -0,0 +1,26 @@
+var id = 1;
+var sum =0;
+function myAdd(){
+    var name = frm.ten.value;
+    var quanti =parseInt(frm.qti.value);
+    var price = parseFloat(frm.pri.value);
+    
+    if (isNaN(quanti)||isNaN(price)||frm.qti.value.trim()===""||frm.pri.value.trim()===""){
+        alert("Bạn đã nhập sai, vui lòng nhập lại");
+        return;
+    }
+        var sub = quanti * price;
+        sub = parseFloat(sub);
+        var row = "<tr>";
+        row += "<td>" + id + "</td>";
+        row += "<td>" + name + "</td>";
+        row += "<td>" + quanti + "</td>";
+        row += "<td>" +price+ '$' + "</td>";
+        row += "<td>" + sub + '$' +"</td>";
+        row += "</tr>";
+        document.getElementById('tbl').innerHTML +=row;
+        sum+=sub
+        id++; 
+        document.getElementById('totalAmount').innerHTML =sum+"$";
+    
+}   
